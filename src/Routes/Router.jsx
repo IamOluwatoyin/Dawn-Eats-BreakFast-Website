a246c88f86b125a7c24ce923a732a87932670cd0
Oluwatoyin Solomon <toyinsolomon4@gmail.com>
2025-09-26 16:34:23 +0100

    first commit

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import LandingPage from "../Pages/LandingPage";
+
+const Router = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+
+          <Rout>              </Rout>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default Router;
