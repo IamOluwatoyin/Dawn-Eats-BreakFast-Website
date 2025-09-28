9f4eb709ff6965bbfbbfa9dc636061010fa1daae
EmekaOmesiete <omesietemicheal@gmail.com>
2025-09-28 03:22:24 +0100

    Installed react icons, moved component folder from assets to src, worked on the header page and the styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./Pages/Header";
 import Router from "./Routes/Router";
 
 const App = () => {
   return (
     <div>
       <Router />
+      <Header/>
     </div>
   );
 };
