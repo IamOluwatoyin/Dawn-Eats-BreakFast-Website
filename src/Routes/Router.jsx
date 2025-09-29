2ddbbbcea7308796e4e4d1847ffb7ebfad677906
Dev Demola <Omiyejuwon Ademola>
2025-09-29 17:48:56 +0100

    updating

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import LandingPage from "../Pages/LandingPage";
+// import LandingPage from "../Pages/LandingPage";
+import DinePage from "../Pages/DinePage";
 
 const Router = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />}></Route>
-
-          {/* <Rout>              </Rout> */}
+          {/* <Route path="/" element={<LandingPage />}></Route> */}
+          <Route path="/" element={<DinePage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
