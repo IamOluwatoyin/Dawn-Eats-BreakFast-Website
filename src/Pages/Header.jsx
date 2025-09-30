d26f2c3f7cc69ce28e661c85660984f8b7d0dc4b
EmekaOmesiete <omesietemicheal@gmail.com>
2025-09-30 01:08:45 +0100

    Worked on the modal components, updated my header and theplacepage pages, did stying for my modal and updated my styling for other components

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,37 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiLocationOn } from "react-icons/ci";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { FaShoppingCart, FaUser, FaSearch } from "react-icons/fa";
 import emekaheaderlogo from "../assets/IMG_4853.JPG";
+import CartModal1 from "../components/Modal1";
 import "./HeaderStyle.css";
 
-const Header = ({ cartCount }) => {
-  return (
-    <header className="emeka_header">
-      <div className="emeka_header_left">
-        <img src={emekaheaderlogo} alt="Logo" className="emeka_header_logo" />
-        <span className="location">
-          <CiLocationOn className="emeka_location" /> Olodi Apapa
-          <RiArrowDropDownLine className="emeka_dropdown" />
-        </span>
-      </div>
+const Header = ({ cartItems = [], setCartItems }) => {
+  
+  const [showModal, setShowModal] = useState(false);
 
-      <div className="emeka_header_right">
-        <div className="search_container">
-          <FaSearch className="search_icon" />
-          <input
-            type="text"
-            placeholder="Search"
-            className="emeka_search_bar"
-          />
+  return (
+    <>
+      <header className="emeka_header">
+        <div className="emeka_header_left">
+          <img src={emekaheaderlogo} alt="Logo" className="emeka_header_logo" />
+          <span className="location">
+            <CiLocationOn className="emeka_location" /> Olodi Apapa
+            <RiArrowDropDownLine className="emeka_dropdown" />
+          </span>
         </div>
-        <div className="emeka_cart_container">
-          <FaShoppingCart className="emeka_icon" />
-          <span className="cart_count">{cartCount}</span>
+
+        <div className="emeka_header_right">
+          <div className="search_container">
+            <FaSearch className="search_icon" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="emeka_search_bar"
+            />
+          </div>
+
+          <div
+            className="emeka_cart_container"
+            onClick={() => setShowModal(true)}
+          >
+            <FaShoppingCart className="emeka_icon" />
+            <span className="cart_count">{cartItems?.length || 0}</span>
+          </div>
+
+          <FaUser className="emeka_icon" />
         </div>
-        <FaUser className="emeka_icon" />
-      </div>
-    </header>
+      </header>
+
+      {showModal && (
+          <CartModal1
+    cartItems={cartItems}
+    onClose={() => setShowModal(false)}
+    onRemoveItem={(id) =>
+      setCartItems((prev) => prev.filter((item) => item.id !== id))
+    }
+    onIncreaseQty={(id) =>
+      setCartItems((prev) =>
+        prev.map((item) =>
+          item.id === id
+            ? { ...item, quantity: (item.quantity || 1) + 1 }
+            : item
+        )
+      )
+    }
+    onDecreaseQty={(id) =>
+      setCartItems((prev) =>
+        prev.map((item) =>
+          item.id === id
+            ? { ...item, quantity: Math.max(1, (item.quantity || 1) - 1) }
+            : item
+        )
+      )
+    }
+  />
+      )}
+    </>
   );
 };
 
--- src/Pages/ThePlaceProdApi.jsx
+++ src/Pages/ThePlaceProdApi.jsx
@@ -18,10 +18,19 @@ const ThePlaceProdApi = ({ cartItems, setCartItems }) => {
     fetchGetProducts();
   }, []);
 
-  const handleAddToCart = (product) => {
-    setCartItems((prev) => [...prev, product]);
-  };
-
+ const handleAddToCart = (product) => {
+  setCartItems((prev) => {
+    const exists = prev.find((item) => item.id === product.id);
+    if (exists) {
+      return prev.map((item) =>
+        item.id === product.id
+          ? { ...item, quantity: (item.quantity || 1) + 1 }
+          : item
+      );
+    }
+    return [...prev, { ...product, quantity: 1 }];
+  });
+};
   const handleRemoveFromCart = (product) => {
     setCartItems((prev) => prev.filter((item) => item.id !== product.id));
   };
